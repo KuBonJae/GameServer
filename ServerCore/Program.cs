876f6fa5b703a7dcc3984abddd00636a08df3e80
KuBonJae <qhswo9@gmail.com>
2025-08-11 18:56:02 +0900

    컴파일러의 최적화로 인한 오류
    - 디버깅 때와는 달리, 릴리즈에서는 컴파일러가 코드를 최적화 해줌으로써 예상치 못한 버그가 발생할 수 있다.

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -5,41 +5,41 @@ namespace ServerCore
 {
     class Program
     {
-        static void MainThread(object? state)
+        // 휘발성 데이터 : 언제 바뀔지 모르니 괜히 최적화 하지 말아라
+        volatile static bool _stop = false; // 전역 변수들은 모든 thread가 공통으로 사용
+        
+        static void ThreadMain()
         {
-            for (int i = 0; i < 5; i++) 
-                Console.WriteLine("Hello Thread");
+            Console.WriteLine("쓰레드 시작");
+
+            // Release 시 코드 최적화로 인해 다음과 같이 변경됨
+            /*
+            if(!_stop)
+            {
+                while(true) { };
+            }
+            */
+
+            while (_stop == false) { };
+
+            Console.WriteLine("쓰레드 종료");
         }
 
         static void Main(string[] args)
         {
-            // Task 자체도 ThreadPool에서 관리함
-            Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning); // 긴 스레드 시간 -> 별도의 스레드 따로 만들어냄
+            Task t = new Task(ThreadMain);
             t.Start();
 
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(5, 5);
+            Thread.Sleep(1000); // 메인 쓰레드는 1초동안 잠시 대기
 
-            for(int i=0; i<4; i++)
-            {
-                ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
-            }
+            _stop = true;
 
-            ThreadPool.QueueUserWorkItem(MainThread);
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중");
+            
+            t.Wait();
 
-            while(true)
-            {
-
-            }
-
-            //Thread t = new Thread(MainThread);
-            //t.Name = "Test Thread";
-            //t.IsBackground = true; // Main쪽에서 멈추면, thread도 멈춘다.
-            //t.Start();
-            //Console.WriteLine("Waiting Thread");
-            //t.Join(); // t Thread를 기다린다.
-
-            //Console.WriteLine("Hello World");
+            Console.WriteLine("종료 성공");
         }
     }
 }
