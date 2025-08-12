bd40560135dda6fbc0ccfacd3ab258522fcbb17f
KuBonJae <qhswo9@gmail.com>
2025-08-12 12:04:24 +0900

    캐시 활용
    - 방금 주문한 곳에서 또 주문이 나올 것이다
    - 방금 주문한 곳 근처에서 주문이 나올 것이다
    
    - 배열을 접근할 때, "순차적으로 접근"(방금 주문한 곳의 근처)하는 쪽이 띄엄띄엄 접근하게 만드는 것 보다 캐싱덕분에 더 빠르다

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -5,41 +5,32 @@ namespace ServerCore
 {
     class Program
     {
-        // 휘발성 데이터 : 언제 바뀔지 모르니 괜히 최적화 하지 말아라
-        volatile static bool _stop = false; // 전역 변수들은 모든 thread가 공통으로 사용
-        
-        static void ThreadMain()
-        {
-            Console.WriteLine("쓰레드 시작");
-
-            // Release 시 코드 최적화로 인해 다음과 같이 변경됨
-            /*
-            if(!_stop)
-            {
-                while(true) { };
-            }
-            */
-
-            while (_stop == false) { };
-
-            Console.WriteLine("쓰레드 종료");
-        }
 
         static void Main(string[] args)
         {
-            Task t = new Task(ThreadMain);
-            t.Start();
+            int[,] arr = new int[10000, 10000];
 
-            Thread.Sleep(1000); // 메인 쓰레드는 1초동안 잠시 대기
+            {
+                long now = DateTime.Now.Ticks;
+                for(int y=0; y<10000; y++)
+                {
+                    for(int x=0; x<10000; x++) 
+                        arr[y, x] = 1;
+                }
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"{end - now}");
+            }
 
-            _stop = true;
-
-            Console.WriteLine("Stop 호출");
-            Console.WriteLine("종료 대기중");
-            
-            t.Wait();
-
-            Console.WriteLine("종료 성공");
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                {
+                    for (int x = 0; x < 10000; x++)
+                        arr[x, y] = 1;
+                }
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"{end - now}");
+            }
         }
     }
 }
