62660bb945787b8d6079193258c12146e8939f53
KuBonJae <qhswo9@gmail.com>
2025-08-12 12:22:44 +0900

    메모리 배리어
    - ram에 데이터를 올리고 가져와 동기화 하는 작업
    - 코드 순서를 하드웨어가 함부로 변경하지 못하도록 함 (서로 연관되어있지 않은 코드는 상황에 따라 순서를 바꿔버리기도 함)

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,36 +1,67 @@
 ﻿using System;
+using System.Runtime.Intrinsics.Arm;
 using System.Threading;
 
 namespace ServerCore
 {
+    // 메모리 배리어
+    // 1. 코드 재배치 억제
+    // 2. 가시성
+
+    // 1. Full Memory Barrier = Store/Load 둘다 막기 (read / write )
+    // 2. Store Memory Barrier = Store만 막기
+    // 3. Load Memory Barrier = Load만 막기
+
     class Program
     {
+        static int x = 0;
+        static int y = 0;
+        static int r1 = 0;
+        static int r2 = 0;
+
+        static void Thread_1()
+        {
+            y = 1;
+
+            Thread.MemoryBarrier(); // 선을 그어버림. y=1; 이 아래로 내려갈 수 없고, r1=x; 가 위로 올라갈 수 없다.
+                                    // 데이터에 내용 쓰기, 내용 불러오기를 해서 동기화 작업을 진행하는 것도 함
+
+            r1 = x;
+            // 순서 연관성이 없기 때문에 하드웨어적으로 다음과 같이 변경될 수도 있다
+            // r1 = x; 
+            // y = 1;
+        }
+
+        static void Thread_2()
+        {
+            x = 1;
+
+            Thread.MemoryBarrier();
+
+
+            r2 = y;
+        }
 
         static void Main(string[] args)
         {
-            int[,] arr = new int[10000, 10000];
-
+            int count = 0;
+            while(true)
             {
-                long now = DateTime.Now.Ticks;
-                for(int y=0; y<10000; y++)
-                {
-                    for(int x=0; x<10000; x++) 
-                        arr[y, x] = 1;
-                }
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"{end - now}");
+                count++;
+                x = y = r1 = r2 = 0;
+
+                Task t1 = new Task(Thread_1);
+                Task t2 = new Task(Thread_2);
+                t1.Start(); 
+                t2.Start();
+
+                Task.WaitAll(t1, t2);
+
+                if (r1 == 0 && r2 == 0)
+                    break;
             }
 
-            {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y < 10000; y++)
-                {
-                    for (int x = 0; x < 10000; x++)
-                        arr[x, y] = 1;
-                }
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"{end - now}");
-            }
+            Console.WriteLine($"{count}번만에 빠져나옴");
         }
     }
 }
