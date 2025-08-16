d60109967d4f7442b15072a16c57ac210a1a10fb
scottku <qhswo9@gmail.com>
2025-08-16 15:59:43 +0900

    커널쪽의 Event를 활용하는 AutoResetEvent
    - 운영체제 단에서 lock 권한을 얻을 수 있는지 연락이 올 때 접근
    - 운영체제와의 연락을 주고받아야 하기에, 기존 방식보다 속도가 확연하게 느림

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,42 +4,30 @@ using System.Threading;
 
 namespace ServerCore
 {
-    class SpinLock
+    class Lock
     {
-        volatile int _locked = 0;
+        // 커널(운영체제)쪽에서 체크하는 bool 값이라고 볼 수 있음
+        AutoResetEvent _available = new AutoResetEvent(true);
+
         public void Acquire()
         {
-            while(true)
-            {
-                //int original = Interlocked.Exchange(ref _locked, 1);
-                //if (original == 0)
-                //    break;
-
-                if (Interlocked.CompareExchange(ref _locked, 1, 0) == 0)
-                    break;
-
-                // 잠시 쉬었다 온다
-                Thread.Sleep(1); // 무조건 일정 시간 강제 휴식
-                Thread.Sleep(0); // 조건부 양보 => 우선순위 낮은 작업에게는 양보 불가능
-                Thread.Yield(); // 관대한 양보 => 모두에게 양보 가능
-            }
+            _available.WaitOne(); // 입장 시도
         }
 
         public void Release()
         {
-            // 본인 스스로만 자물쇠를 쥐고 있는게 확실한 상황이므로, 직접적으로 데이터를 건드려도 문제가 전혀 없다.
-            _locked = 0;
+            _available.Set();
         }
     }
 
     class Program
     {
         static int _num = 0;
-        static SpinLock _lock = new SpinLock();
+        static Lock _lock = new Lock();
 
         static void Thread1()
         {
-            for(int i=0; i < 100000;i++)
+            for(int i=0; i < 10000;i++)
             {
                 _lock.Acquire();
                 _num++;
@@ -49,7 +37,7 @@ namespace ServerCore
 
         static void Thread2()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 10000; i++)
             {
                 _lock.Acquire();
                 _num--;
