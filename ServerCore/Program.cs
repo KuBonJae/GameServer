9760f75296521f00b37b73c513a34d913b0b8698
scottku <qhswo9@gmail.com>
2025-08-16 16:03:51 +0900

    AutoResetEvent처럼 커널단과 연결되는 Mutex
    - 내부에 ThreadID 등을 통해 다른 Thread에서 lock이 해제되는 것을 막는 등 추가 기능들이 포함
    - 물론 추가 기능들이 있는 만큼 조금 더 무겁다고 볼 수 있다.

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,34 +4,18 @@ using System.Threading;
 
 namespace ServerCore
 {
-    class Lock
-    {
-        // 커널(운영체제)쪽에서 체크하는 bool 값이라고 볼 수 있음
-        AutoResetEvent _available = new AutoResetEvent(true);
-
-        public void Acquire()
-        {
-            _available.WaitOne(); // 입장 시도
-        }
-
-        public void Release()
-        {
-            _available.Set();
-        }
-    }
-
     class Program
     {
         static int _num = 0;
-        static Lock _lock = new Lock();
+        static Mutex _lock = new Mutex();
 
         static void Thread1()
         {
             for(int i=0; i < 10000;i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num++;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
@@ -39,9 +23,9 @@ namespace ServerCore
         {
             for (int i = 0; i < 10000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num--;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
         static void Main(string[] args)
