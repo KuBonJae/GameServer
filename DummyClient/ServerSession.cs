a5621d8f15136db47352ebd543a9d7e3e984c3c4
KuBonJae <qhswo9@gmail.com>
2025-09-15 16:28:46 +0900

    리스트 보내기
    - string과 마찬가지로 먼저 리스트 내부 갯수를 보낸다.
    - 그 갯수 만큼 for문을 돌면서 하나씩 역직렬화 하는 방식

--- DummyClient/ServerSession.cs
+++ DummyClient/ServerSession.cs
@@ -22,6 +22,38 @@ namespace DummyClient
         public long playerID;
         public string name;
 
+        public struct SkillInfo
+        {
+            public int id;
+            public short level;
+            public float duration;
+
+            public bool Write(Span<byte> s, ref ushort count)
+            {
+                bool success = true;
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.id);
+                count += sizeof(int);
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.level);
+                count += sizeof(short);
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.duration);
+                count += sizeof(float);
+
+                return true;
+            }
+
+            public void Read(ReadOnlySpan<byte> s, ref ushort count)
+            {
+                this.id = BitConverter.ToInt32(s.Slice(count, s.Length - count));
+                count += sizeof(int);
+                this.level = BitConverter.ToInt16(s.Slice(count, s.Length - count));
+                count += sizeof(short);
+                this.duration = BitConverter.ToSingle(s.Slice(count, s.Length - count));
+                count += sizeof(float);
+            }
+        }
+
+        public List<SkillInfo> skills = new List<SkillInfo>();
+
         public PlayerInfoReq()
         {
             this.packetID = (ushort)PacketID.PlayerInfoReq;
@@ -45,6 +77,17 @@ namespace DummyClient
             ushort nameLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
             count += sizeof(ushort);
             this.name = Encoding.Unicode.GetString(s.Slice(count, nameLen));
+            count += nameLen;
+
+            // skill list
+            ushort skillLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
+            count += sizeof(ushort);
+            for(int i=0;i<skillLen; i++)
+            {
+                SkillInfo skill = new SkillInfo();
+                skill.Read(s, ref count);
+                skills.Add(skill);
+            }
         }
 
         public override ArraySegment<byte> Write()
@@ -69,6 +112,14 @@ namespace DummyClient
             success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), nameLen);
             count += sizeof(ushort);
             count += nameLen;
+
+            // skill List
+            success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), (ushort)skills.Count);
+            count += sizeof(ushort);
+            foreach(SkillInfo skillInfo in skills)
+            {
+                success &= skillInfo.Write(span, ref count);
+            }
 
             // 사이즈 크기는 마지막까지 체크하고 확인해야 함
             success &= BitConverter.TryWriteBytes(span, count);
@@ -100,6 +151,10 @@ namespace DummyClient
             Console.WriteLine($"OnConnected bytes : {endPoint}");
 
             PlayerInfoReq packet = new PlayerInfoReq() { playerID = 1001, name = "My Lord!" };
+            packet.skills.Add(new PlayerInfoReq.SkillInfo() { id = 101, level = 1, duration = 3.0f });
+            packet.skills.Add(new PlayerInfoReq.SkillInfo() { id = 102, level = 2, duration = 4.0f });
+            packet.skills.Add(new PlayerInfoReq.SkillInfo() { id = 103, level = 3, duration = 5.0f });
+            packet.skills.Add(new PlayerInfoReq.SkillInfo() { id = 104, level = 4, duration = 6.0f });
 
             ArraySegment<byte> s = packet.Write();
 
--- Server/ClientSession.cs
+++ Server/ClientSession.cs
@@ -22,6 +22,38 @@ namespace Server
         public long playerID;
         public string name;
 
+        public struct SkillInfo
+        {
+            public int id;
+            public short level;
+            public float duration;
+
+            public bool Write(Span<byte> s, ref ushort count)
+            {
+                bool success = true;
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.id);
+                count += sizeof(int);
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.level);
+                count += sizeof(short);
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.duration);
+                count += sizeof(float);
+
+                return true;
+            }
+
+            public void Read(ReadOnlySpan<byte> s, ref ushort count)
+            {
+                this.id = BitConverter.ToInt32(s.Slice(count, s.Length - count));
+                count += sizeof(int);
+                this.level = BitConverter.ToInt16(s.Slice(count, s.Length - count));
+                count += sizeof(short);
+                this.duration = BitConverter.ToSingle(s.Slice(count, s.Length - count));
+                count += sizeof(float);
+            }
+        }
+
+        public List<SkillInfo> skills = new List<SkillInfo>();
+
         public PlayerInfoReq()
         {
             this.packetID = (ushort)PacketID.PlayerInfoReq;
@@ -45,6 +77,17 @@ namespace Server
             ushort nameLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
             count += sizeof(ushort);
             this.name = Encoding.Unicode.GetString(s.Slice(count, nameLen));
+            count += nameLen;
+
+            // skill list
+            ushort skillLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
+            count += sizeof(ushort);
+            for (int i = 0; i < skillLen; i++)
+            {
+                SkillInfo skill = new SkillInfo();
+                skill.Read(s, ref count);
+                skills.Add(skill);
+            }
         }
 
         public override ArraySegment<byte> Write()
@@ -65,11 +108,18 @@ namespace Server
 
             // string -> 유니티는 일단 UTF-16이 기본
             // string의 length를 2 byte -> string 전달
-            ushort nameLen = (ushort)Encoding.Unicode.GetByteCount(this.name);
+            ushort nameLen = (ushort)Encoding.Unicode.GetBytes(this.name, 0, this.name.Length, openSegment.Array, openSegment.Offset + count + sizeof(ushort));
             success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), nameLen);
             count += sizeof(ushort);
-            Array.Copy(Encoding.Unicode.GetBytes(this.name), 0, openSegment.Array, count, nameLen);
             count += nameLen;
+
+            // skill List
+            success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), (ushort)skills.Count);
+            count += sizeof(ushort);
+            foreach (SkillInfo skillInfo in skills)
+            {
+                success &= skillInfo.Write(span, ref count);
+            }
 
             // 사이즈 크기는 마지막까지 체크하고 확인해야 함
             success &= BitConverter.TryWriteBytes(span, count);
@@ -122,6 +172,12 @@ namespace Server
                     PlayerInfoReq p = new PlayerInfoReq();
                     p.Read(buffer);
                     Console.WriteLine($"PlayerInfoReq : {p.playerID} / {p.name}");
+
+                    foreach(PlayerInfoReq.SkillInfo skill in p.skills)
+                    {
+                        Console.WriteLine($"Skill {skill.id} / {skill.level} / {skill.duration}");
+                    }
+
                     break;
 
                 case PacketID.PlayerinfoOk:
