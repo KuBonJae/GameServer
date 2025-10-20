94d108de3c35ebe17839a3b44bee2dc92bb89811
KuBonJae <qhswo9@gmail.com>
2025-10-20 15:00:52 +0900

    [feat] 패킷 생성기 추가
    패킷 업데이트마다 bat 파일 재실행을 통해 서버/클라 패킷 내용을 업데이트한다

---
+++ Common/Packet/GenPackets.bat
@@ -0,0 +1,3 @@
+START ..\..\PacketGenerator\bin\Debug\PacketGenerator.exe ..\..\PacketGenerator\PDL.xml
+XCOPY GenPackets.cs ..\..\DummyClient\Packet /Y 
+XCOPY GenPackets.cs ..\..\Server\Packet /Y 
