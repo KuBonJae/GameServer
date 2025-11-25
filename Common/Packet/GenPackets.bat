10b7cec3b315587eb2d4ed1d69039be63d451de6
KuBonJae <scottku@spellbinds.io>
2025-11-25 18:37:18 +0900

    패킷매니저 제작 자동화
    - 모든 패킷을 다 등록하지 않고, 각자 쓸 패킷만 골라서 등록시킨다.
    - 패킷 사용 주체에 따른 Register 분할 등록

--- Common/Packet/GenPackets.bat
+++ Common/Packet/GenPackets.bat
@@ -1,3 +1,5 @@
 START  /WAIT ..\..\PacketGenerator\bin\Debug\PacketGenerator.exe ..\..\PacketGenerator\PDL.xml
 XCOPY GenPackets.cs ..\..\DummyClient\Packet /Y 
 XCOPY GenPackets.cs ..\..\Server\Packet /Y 
+XCOPY ClientPacketManager.cs ..\..\DummyClient\Packet /Y 
+XCOPY ServerPacketManager.cs ..\..\Server\Packet /Y 
--- DummyClient/ServerSession.cs
+++ DummyClient/ServerSession.cs
@@ -21,11 +21,11 @@ namespace DummyClient
         {
             Console.WriteLine($"OnConnected bytes : {endPoint}");
 
-            PlayerInfoReq packet = new PlayerInfoReq() { playerId = 1001, name = "My Lord!" };
-            packet.skills.Add(new PlayerInfoReq.Skill() { id = 101, level = 1, duration = 3.0f });
-            packet.skills.Add(new PlayerInfoReq.Skill() { id = 102, level = 2, duration = 4.0f });
-            packet.skills.Add(new PlayerInfoReq.Skill() { id = 103, level = 3, duration = 5.0f });
-            packet.skills.Add(new PlayerInfoReq.Skill() { id = 104, level = 4, duration = 6.0f });
+            C_PlayerInfoReq packet = new C_PlayerInfoReq() { playerId = 1001, name = "My Lord!" };
+            packet.skills.Add(new C_PlayerInfoReq.Skill() { id = 101, level = 1, duration = 3.0f });
+            packet.skills.Add(new C_PlayerInfoReq.Skill() { id = 102, level = 2, duration = 4.0f });
+            packet.skills.Add(new C_PlayerInfoReq.Skill() { id = 103, level = 3, duration = 5.0f });
+            packet.skills.Add(new C_PlayerInfoReq.Skill() { id = 104, level = 4, duration = 6.0f });
 
             ArraySegment<byte> s = packet.Write();
 
